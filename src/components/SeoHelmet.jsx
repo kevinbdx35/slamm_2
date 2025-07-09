e35d5bb9d152ef7fd07181f5ab1fdb25b193b12b
Kevin <k.bardoux@orange.fr>
2025-07-09 19:28:12 +0200

    update menu

--- src/components/SeoHelmet.jsx
+++ src/components/SeoHelmet.jsx
@@ -1,20 +1,32 @@
 import { Helmet } from 'react-helmet';
 
-export default function SeoHelmet({ title, description, url, image }) {
+export default function SeoHelmet({
+  title = 'SLAMM MMA',
+  description = 'Découvre le MMA à Saint-Lunaire avec le club SLAMM. Entraînements, actualités et esprit combatif !',
+  url = 'https://mma-saint-lunaire.fr',
+  image = 'https://mma-saint-lunaire.fr/img/og-image.jpg',
+}) {
   return (
     <Helmet>
+      {/* Viewport mobile responsive */}
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover"
+      />
+
+      {/* SEO */}
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="canonical" href={url} />
 
-      {/* Open Graph */}
+      {/* Open Graph (Facebook, LinkedIn...) */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
       <meta property="og:url" content={url} />
       <meta property="og:type" content="website" />
 
-      {/* Twitter */}
+      {/* Twitter Card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
