e205526e207d587274ddd31ffaa8f753dffcc89b
Kevin <k.bardoux@orange.fr>
2026-03-26 18:08:47 +0000

    deploy: 9c2323394baa75ae15d0245e60acb2ad2c2eb1bb

---
+++ _astro/urls.TuLnIsUH.js
@@ -0,0 +1,6 @@
+import{c as t}from"./createLucideIcon.DwOHoU2G.js";/**
+ * @license lucide-react v0.563.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["path",{d:"M15 3h6v6",key:"1q9fwt"}],["path",{d:"M10 14 21 3",key:"gplh6r"}],["path",{d:"M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6",key:"a6xqqp"}]],e=t("external-link",a),o={TRIAL_BOOKING:"https://slamm.assoconnect.com/collect/description/586837-g-cours-d-essai"};export{o as A,e as E};
